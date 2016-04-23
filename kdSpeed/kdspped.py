bb390e98472d691fc2c7d81485f64586fb2d14b9
Peter Yoachim <yoachim@uw.edu>
2016-04-22 22:51:29 -0700

    bah, new one is faster

--- kdSpeed/kdspped.py
+++ kdSpeed/kdspped.py
@@ -1,11 +1,11 @@
 import numpy as np
 import scipy
 from scipy.spatial import cKDTree as kdtree
+import healpy as hp
 
 # Check the speed of the scipy kdtree
 
 def test_tree_time():
-	
 
 	def tree_xyz(lat, lon):
 		"""
@@ -16,24 +16,25 @@ def test_tree_time():
 		z = np.sin(lat)
 		return x, y, z
 
-	npts = 2e6
+	npts = 1e6
 	np.random.seed(42)
-	# random points on a sphere. Not uniform.
-	lat = np.random.uniform(low=-np.pi/2., high=np.pi/2., size=npts)
+	# random points on a sphere. 
+	lat = np.arccos(2.*np.random.uniform(low=0, high=1, size=npts) - 1.) - np.pi/2.
 	lon = np.random.uniform(low=0, high=2*np.pi, size=npts)
 	x, y, z = tree_xyz(lat, lon)
 
 	# Postiions to search on
-	position_lon = np.linspace(0., 2.*np.pi, 200.)
-	position_lat = np.zeros(position_lon.size)
+	nside = 128
+	position_lat, position_lon = hp.pix2ang(nside, np.arange(hp.nside2npix(nside)))
+	position_lat = np.pi/2. - position_lat
 
 	pos_x, pos_y, pos_z = tree_xyz(position_lat, position_lon)
 
 	leafsize = 100
 	tree = kdtree(zip(x,y,z), leafsize=leafsize)
 
-	# Set a 3 degree search radius
-	radius = 3.
+	# Set a search radius
+	radius = 2.
 	x0, y0, z0 = (1, 0, 0)
 	x1, y1, z1 = tree_xyz(np.radians(radius), 0)
 	search_rad = np.sqrt((x1-x0)**2+(y1-y0)**2+(z1-z0)**2)
