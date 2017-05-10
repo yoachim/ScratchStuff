d07ba11dd16a4d5cf03acd3928ae1d691d22fc32
Peter Yoachim <yoachim@uw.edu>
2017-05-10 15:36:41 -0700

    fucking radians

--- Roth2MAF/make_sql.py
+++ Roth2MAF/make_sql.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from lsst.sims.maf.utils import obs2sqlite
+from lsst.sims.maf.utils.obs2sqlite import obs2sqlite
 import numpy as np
 
 
@@ -12,5 +12,6 @@ data = np.loadtxt(filename, skiprows=1, delimiter=',', dtype=zip(names, types))
 data['mjd'] = data['mjd']/86400. + 40587
 print('Converting %i obserations to sqlite' %np.size(data))
 
-obs2sqlite(data)
+#obs2sqlite(data[0:100], outfile='test.sqlite')
 
+obs2sqlite(data)
