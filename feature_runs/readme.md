9c182623f09e86f512e017dc0dd81914346b3e50
Peter Yoachim <yoachim@uw.edu>
2017-11-30 14:24:34 -0800

    camera on works

---
+++ feature_runs/readme.md
@@ -0,0 +1,2 @@
+
+A directory to run some quick simulations for doing some cadence simulations.
---
+++ feature_runs/run_glance.py
@@ -0,0 +1,20 @@
+import lsst.sims.maf.batches as batches
+import lsst.sims.maf.db as db
+from lsst.sims.maf.metricBundles import MetricBundleGroup
+
+colmap = batches.ColMapDict('barebones')
+colmap['ra'] = 'RA'
+colmap['seeingEff'] = 'FWHMeff'
+colmap['seeingGeom'] = 'FWHM_geometric'
+bd = batches.glanceBatch(colmap=colmap)
+
+
+def run_glance(outDir, dbname):
+    conn = db.Database(dbname)
+    resultsDb = db.ResultsDb(outDir=outDir)
+    mbg = MetricBundleGroup(bd, conn, outDir=outDir, resultsDb=resultsDb)
+    mbg.runAll()
+    mbg.plotAll()
+    conn.close()
+
+run_glance('temp_2', 'feature_baseline_1yrs.db')
